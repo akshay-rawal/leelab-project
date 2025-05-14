c731e6114cc00de0999b1d50fa0a42f1e7abb945
Akshay Rawal <akshayrawal7698@gmail.com>
2025-05-14 07:40:21 +0530

    basic structure of Login,Signup and Home page is ready

--- frontend/leetcode/src/App.jsx
+++ frontend/leetcode/src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
-import { Router,Route,Navigate } from 'react-router-dom'
+import React from "react";
+import { Routes, Route, Navigate } from "react-router-dom";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 
 const App = () => {
   return (
- <>
- 
- </> )
-}
+    <div className="flex flex-col items-center justify-start">
+      <Routes>
+        <Route path="/login" element={<Login/>}></Route>
+        <Route path="/sign-up" element={<Signup/>}></Route>
+        <Route path="/" element={<Home/>}></Route>
+      </Routes>
+    </div>
+  );
+};
 
-export default App
+export default App;
