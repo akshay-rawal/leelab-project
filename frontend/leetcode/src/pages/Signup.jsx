01b9f2576e59395d23a538a3be857d193c0aa0ac
Akshay Rawal <akshayrawal7698@gmail.com>
2025-05-14 23:00:12 +0530

    install zod and ready zodSchema validator

--- frontend/leetcode/src/pages/Signup.jsx
+++ frontend/leetcode/src/pages/Signup.jsx
@@ -1,8 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
+import SignupSchema from './schemas/SignupSchemas';
 import {Code,Eye,EyeOff,Loader2,Lock,Mail} from "lucide-react"
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
 
 
 const Signup = () => {
+   const [showPassword,setShowPassword] = useState(false)
+   const formMethods = useForm({
+  resolver:zodResolver(SignupSchema )
+})
+
+const { register, handleSubmit, formState } = formMethods;
+const { errors } = formState;
+
+ const onSubmit = async (data) => {
+    console.log(data);
+ }
+
   return (
     <div>Signup</div>
   )
--- frontend/leetcode/src/pages/schemas/SignupSchemas.jsx
+++ frontend/leetcode/src/pages/schemas/SignupSchemas.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
-import z from "zod"
+import {z}from "zod"
 
 import React from 'react'
 
-const SignupSchemas = () => {
-  return (
-    <div>SignupSchemas</div>
-  )
-}
-
-export default SignupSchemas
+export const SignupSchema = z.object({
+  name: z
+    .string()
+    .min(2, { message: "Name must be at least 2 characters long" }),
+  email: z
+    .string()
+    .email({ message: "Invalid email address" }),
+  password: z
+    .string()
+    .min(6, { message: "Password must be at least 6 characters long" }),
+  confirmPassword: z
+    .string()
+}).refine((data) => data.password === data.confirmPassword, {
+  message: "Passwords do not match",
+  path: ["confirmPassword"], // Error will show at confirmPassword field
+});
+export default SignupSchema
