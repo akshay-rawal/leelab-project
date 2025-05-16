180f0b03198e0b67941b7698eb23e8c1f961f578
Akshay Rawal <akshayrawal7698@gmail.com>
2025-05-16 22:57:03 +0530

    store,toast,axios.js and more

--- frontend/leetcode/src/App.jsx
+++ frontend/leetcode/src/App.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
+import { Toaster } from "react-hot-toast";
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
+import { useAuthStore } from "./store/store";
 
 const App = () => {
-  let authUser = null;
+  const {authUser, isCheckingAuth,checkAuth} = useAuthStore()
+
   return (
+ 
     <div className="flex flex-col items-center justify-start">
+          <Toaster />
       <Routes>
         <Route path="/login" element={!authUser?<Login/>: <Navigate to={"/"}/>}></Route>
         <Route path="/sign-up" element={!authUser?<Signup/> : <Navigate to={"/"}/>}></Route>
